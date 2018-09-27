b5d3b4d6e78c6e884c40f2212217dcc8eaad493d
Andreas Høiberg Eike <andree18@student.uia.no>
2018-09-27 12:12:07 +0200

    Added Assignment 4_1

---
+++ assignment_4_1/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include  <ctype.h>
+
+#define STRING_LENGTH 100
+
+int reverseCheck(char ord[]) {
+    for (int i=0;i<strlen(ord)/2;i++){
+        if(ord[i]==ord[strlen(ord)-i-1])
+            return 1;
+        else
+            return 0;
+            break;
+    }
+}
+void reverse(char ord[]) {
+    char reverseOrd[STRING_LENGTH] = {0};
+    printf("The word reversed is '");
+
+    for (int i = strlen(ord) - 1; i >= 0; i--) {
+        printf("%c", ord[i]);
+        reverseOrd[strlen(ord) - i - 1] = ord[i];
+    }
+    printf("'\n");
+
+}
+
+int main() {
+    char ord[STRING_LENGTH]={0};
+    printf("Skriv inn et ord: ");
+    fgets(ord,STRING_LENGTH,stdin);
+    ord[strlen(ord)-1]=0;
+    printf("The word contains %i letters\n",strlen(ord));
+    reverse(ord);
+    if (reverseCheck(ord)==1)
+        printf("The word is a palindrome");
+    else
+        printf("The word is not a palindrome");
+    return 0;
+}
