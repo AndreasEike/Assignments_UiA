78d90734453ae9a17934c83fe690db55ecd5fb30
Andreas Høiberg Eike <andree18@student.uia.no>
2018-10-30 19:53:28 +0100

    Edited assignment 6_2

--- assignment_6_2/main.c
+++ assignment_6_2/main.c
@@ -8,8 +8,8 @@ int max(student_t student[],int count) {
     int age=0;
     int index=0;
 
-    for (int i=0;i<=count;i++) {
-        if (student[i].age>age)
+    for (int i=0;i<count;i++) {
+        if (student[i].age>=age)
             index=i;
     }
     return index;
@@ -20,7 +20,7 @@ int min(student_t student[],int count) {
     int age=student[0].age;
     int index=0;
     for (int i=0;i<count;i++) {
-        if (student[i].age<age)
+        if (student[i].age<=age)
             index=i;
     }
     return index;
